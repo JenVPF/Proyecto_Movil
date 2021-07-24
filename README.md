e33d6d2b5f4a6491fdbaa9f8c8b28433fb72a498
Jennifer <jennifer.portinof@utem.cl>
2021-07-23 20:21:11 -0400

    Plantilla de ionic

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Proyecto_Movil
+
 App Movil para el ramo de CPyD
+
+### Dependencias
+- Instalacion de ionic
+npm install -g @ionic/cli
+
+### Orden de archivos
+- src/pages: Estan las vistas de la app 
+- src/App.tsx: Es el menu para moverse entre las vistas
---
+++ myApp/src/pages/Tab1.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Sismos</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
